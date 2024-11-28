e958f927569491e4eabe7959c144aed0a768ecaa
clar1sseee <tiongsonclarisse@gmail.com>
2024-11-28 19:23:00 +0100

    updated after 1st projectmeeting

--- popup.js
+++ popup.js
@@ -1,14 +1,35 @@
-document.getElementById("analyzeButton").addEventListener("click", () => {
-    chrome.tabs.executeScript(
-        {
-            code: "window.getSelection().toString();",
-        },
-        (selection) => {
-            const selectedText = selection[0];
-            if (!selectedText) {
-                document.getElementById("result").textContent = "Bitte Text markieren.";
-                return;
-            }
+console.log("dummy1");
+document
+  .getElementById("analyzeButton")
+  .addEventListener("click", async function handleClickAnalyzeButton() {
+    console.log("dummy2");
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        const selectedText = window.getSelection().toString();
+        console.log("dummy3", selectedText);
+        if (!selectedText) {
+          document.getElementById("result").textContent = selectedText;
+          console.log("selcted text", selectedText);
+          return;
         }
-    );
-});
+      },
+    });
+    // chrome.scripting.executeScript(
+    //     {
+    //         code: "window.getSelection().toString();",
+    //     },
+    //     (selection) => {
+    //         const selectedText = selection[0];
+    //         if (!selectedText) {
+    //             document.getElementById("result").textContent = selectedText;
+    //             console.log("selcted text", selectedText);
+    //             return;
+    //         }
+    //     }
+    // );
+  });
