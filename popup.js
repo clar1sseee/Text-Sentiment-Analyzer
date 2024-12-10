ffeccfd81c5032f420c1c7f887f77178f7fbc2a0
clar1sseee <tiongsonclarisse@gmail.com>
2024-12-10 12:43:35 +0100

    console.log removed, new popup.js

--- popup.js
+++ popup.js
@@ -1,35 +1,42 @@
-console.log("dummy1");
 document
   .getElementById("analyzeButton")
   .addEventListener("click", async function handleClickAnalyzeButton() {
-    console.log("dummy2");
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
     });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        const selectedText = window.getSelection().toString();
-        console.log("dummy3", selectedText);
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        func: () => {
+          const selectedText = window.getSelection().toString();
+          return selectedText;
+        },
+      },
+      (injectionResults) => {
+        const selectedText = injectionResults[0]?.result;
+
         if (!selectedText) {
-          document.getElementById("result").textContent = selectedText;
-          console.log("selcted text", selectedText);
+          document.getElementById("result").textContent = "No text selected.";
           return;
         }
-      },
-    });
-    // chrome.scripting.executeScript(
-    //     {
-    //         code: "window.getSelection().toString();",
-    //     },
-    //     (selection) => {
-    //         const selectedText = selection[0];
-    //         if (!selectedText) {
-    //             document.getElementById("result").textContent = selectedText;
-    //             console.log("selcted text", selectedText);
-    //             return;
-    //         }
-    //     }
-    // );
+        document.getElementById(
+          "result"
+        ).textContent = `Selected text: ${selectedText}`;
+        console.log("Selected text:", selectedText);
+      }
+    );
   });
+// chrome.scripting.executeScript(
+//     {
+//         code: "window.getSelection().toString();",
+//     },
+//     (selection) => {
+//         const selectedText = selection[0];
+//         if (!selectedText) {
+//             document.getElementById("result").textContent = selectedText;
+//             console.log("selcted text", selectedText);
+//             return;
+//         }
+//     }
+// );
